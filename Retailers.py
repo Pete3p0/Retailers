6a49137a18417f84e77f004714db6fd7dfac296c
Pete3p0 <61388515+Pete3p0@users.noreply.github.com>
2020-11-27 15:48:39 +0200

    Update Retailers.py
    Outdoor Warehouse summary

--- Retailers.py
+++ Retailers.py
@@ -1248,7 +1248,8 @@ elif option == 'Outdoor-Warehouse':
         # Get retailers map
         df_ow_retailers_map = df_map
         df_ow_retailers_map = df_ow_retailers_map.rename(columns={'SKUCode': 'Article Code'})
-        df_retailers_map_ow_final = df_ow_retailers_map[['Article Code','SMD Code','RSP']]
+        df_ow_retailers_map = df_ow_retailers_map.rename(columns={'Description': 'Product Description'})
+        df_retailers_map_ow_final = df_ow_retailers_map[['Article Code','SMD Code','Product Description','RSP']]
 
         # Get retailer data
         df_ow_data = df_data
@@ -1316,7 +1317,7 @@ elif option == 'Outdoor-Warehouse':
         st.table(df_missing_unique_2)
 
     except:
-        st.markdown("**Retailer map column headings:** SKUCode,SMD Code,RSP")
+        st.markdown("**Retailer map column headings:** Article Code, SMD Code, Description ,RSP")
         st.markdown("**Retailer data column headings:** Code, Product, SKUCode")
         st.markdown("Column headings are **case sensitive.** Please make sure they are correct")
 
@@ -1333,10 +1334,33 @@ elif option == 'Outdoor-Warehouse':
 
         # Don't change these headings. Rather change the ones above
         final_df_ow = df_ow_merged[['Start Date','SKU No.', 'Product Code', 'Forecast Group','Store Name','SOH Qty','Sales Qty','Total Amt']]
+        final_df_ow_p = df_ow_merged[['Product Code','Product Description','Total Amt']]
+        final_df_ow_s = df_ow_merged[['Store Name','Total Amt']]    
 
         # Show final df
         total = final_df_ow['Total Amt'].sum()
         st.write('The total sales for the week are: R',"{:0,.2f}".format(total).replace(',', ' '))
+        st.write('')
+        st.write('Top 10 products for the week:')
+        grouped_df_pt = final_df_ow_p.groupby("Product Description").sum().sort_values("Total Amt", ascending=False)
+        grouped_df_final_pt = grouped_df_pt[['Total Amt']].head(10)
+        st.dataframe(grouped_df_final_pt.style.set_precision(2).format('R{0:,.2f}'),width=5000)
+        st.write('')
+        st.write('Top 10 stores for the week:')
+        grouped_df_st = final_df_ow_s.groupby("Store Name").sum().sort_values("Total Amt", ascending=False)
+        grouped_df_final_st = grouped_df_st[['Total Amt']].head(10)
+        st.dataframe(grouped_df_final_st.style.set_precision(2).format('R{0:,.2f}'),width=5000)
+        st.write('')
+        st.write('Bottom 10 products for the week:')
+        grouped_df_pb = final_df_ow_p.groupby("Product Description").sum().sort_values("Total Amt", ascending=False)
+        grouped_df_final_pb = grouped_df_pb[['Total Amt']].tail(10)
+        st.dataframe(grouped_df_final_pb.style.set_precision(2).format('R{0:,.2f}'),width=5000)
+        st.write('')
+        st.write('Bottom 10 stores for the week:')
+        grouped_df_sb = final_df_ow_s.groupby("Store Name").sum().sort_values("Total Amt", ascending=False)
+        grouped_df_final_sb = grouped_df_sb[['Total Amt']].tail(10)
+        st.dataframe(grouped_df_final_sb.style.set_precision(2).format('R{0:,.2f}'),width=5000)
+        st.write('Final Dataframe:')           
         final_df_ow
 
         # Output to .xlsx
